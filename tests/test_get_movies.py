9656590a8ed1721950d54636f182be567e67e0fb
mishasomov <somovmykhailo@gmail.com>
2022-07-03 00:29:08 +0200

    fixed  typo in tests names

--- tests/test_get_movies.py
+++ tests/test_get_movies.py
@@ -3,6 +3,6 @@ def test_get_top_rated_movies(top_rated_movies_controller):
     assert resp.status_code == 200
 
 
-def test_get_top_rated_movies_being_unathorized(top_rated_movies_controller):
+def test_get_top_rated_movies_being_unauthorized(top_rated_movies_controller):
     resp = top_rated_movies_controller.get_movies_unauthorized()
     assert resp.status_code == 401
--- tests/test_rate_movie.py
+++ tests/test_rate_movie.py
@@ -3,7 +3,7 @@ def test_movie_can_be_rated(rate_movie_controller):
     assert respns.status_code == 201
 
 
-def test_unable_to_rate_as_unathorised(rate_movie_controller):
+def test_unable_to_rate_as_unauthorised(rate_movie_controller):
     respns = rate_movie_controller.rate_movie_as_unauth(id='271236', value=10)
     assert respns.status_code == 401
 
@@ -18,4 +18,3 @@ def test_response_401_schema(rate_movie_controller, validator, schemas):
     respons_schema = respns.as_dict
     actual_validation_result = validator.validate(respons_schema, schemas.TOP_RATED_401)
     assert actual_validation_result is True
-
