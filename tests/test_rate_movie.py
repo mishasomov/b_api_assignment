46de9359574838fd6c59615e39194ac5be2ffe06
mishasomov <somovmykhailo@gmail.com>
2022-07-04 21:22:49 +0200

    added two boundary tests

--- tests/test_rate_movie.py
+++ tests/test_rate_movie.py
@@ -1,12 +1,32 @@
 import allure
 from assertpy import assert_that
 import pytest
+from data.messages import Messages
+
 
 @allure.severity(allure.severity_level.CRITICAL)
 @pytest.mark.rate_movie_endpoint
 def test_movie_can_be_rated(rate_movie_controller):
-    response = rate_movie_controller.rate_movie(id='271236', value=10)
-    assert response.status_code == 201
+    response = rate_movie_controller.rate_movie(id='271236', value=2)
+    assert_that(response.status_code).is_equal_to(201)
+
+
+@allure.severity(allure.severity_level.CRITICAL)
+@pytest.mark.rate_movie_endpoint
+def test_movie_cant_be_rated_with_value_less_than_boundary(rate_movie_controller):
+    response = rate_movie_controller.rate_movie(id='271236', value=-1)
+    msg = response.as_dict
+    assert_that(response.status_code).is_equal_to(400)
+    assert_that(msg["status_message"]).is_equal_to(Messages.VALUE_TOO_LOW)
+
+
+@allure.severity(allure.severity_level.CRITICAL)
+@pytest.mark.rate_movie_endpoint
+def test_movie_cant_be_rated_with_value_greater_than_boundary(rate_movie_controller):
+    response = rate_movie_controller.rate_movie(id='271236', value=20)
+    msg = response.as_dict
+    assert_that(response.status_code).is_equal_to(400)
+    assert_that(msg["status_message"]).is_equal_to(Messages.VALUE_TOO_HIGH)
 
 
 @allure.severity(allure.severity_level.CRITICAL)
